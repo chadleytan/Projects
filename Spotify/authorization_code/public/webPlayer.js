4bb136767b67a3d43af8114bab2cb2d05f3224e8
ChadleyTan <chadley.tan@mail.utoronto.ca>
2020-04-05 17:53:50 -0400

    search get request is working

--- Spotify/authorization_code/public/webPlayer.js
+++ Spotify/authorization_code/public/webPlayer.js
@@ -1,7 +1,8 @@
 "use strict"
 
-function startWebPlayer(access_token){
+function StartWebPlayer(access_token){
     console.log("Starting Web API");
+ 
     window.onSpotifyWebPlaybackSDKReady = () => {
         const token = access_token;
         const player = new Spotify.Player({
@@ -16,24 +17,30 @@ function startWebPlayer(access_token){
         player.addListener('playback_error', ({ message }) => { console.error(message); });
 
         // Playback status updates
-        player.on('player_state_changed', state => {
-            console.log(state)
-            $('#current-track').attr('src', state.track_window.current_track.album.images[0].url);
-            $('#current-track-name').text(state.track_window.current_track.name);
-            $('#track-length').text(calculateLength(state.duration));
-        });
+        player.addListener('player_state_changed', state => { console.log(state); });
 
         // Ready
         player.addListener('ready', data => {
-        console.log('Ready with Device ID', data.device_id);
-        
-            // Play a track using our new device ID
-            play(data.device_id, access_token);
+            console.log('Ready with Device ID', data.device_id);
+            console.log(data);
 
+            $('#submit-search').click(function() {
+                var q_search = document.getElementById("search-track").value;
+                SearchTrack(q_search, access_token);
+            });
+
+            // Play a track using our new device ID
+            $("#play-track").click(function() {
+                //var trackid = 
+                Play(data.device_id, access_token);
+                console.log("trying to play track");
+            })
+            
+            // On click skips to position in track given by time
             $("#skip-pos").click(function(){
                 var time = document.getElementById("time-skip").value;
                 console.log(time);
-                skipToPosition(player, time);
+                SkipToPosition(player, time);
             });
         });
 
@@ -43,35 +50,43 @@ function startWebPlayer(access_token){
         });
 
         // Connect to the player!
-        player.connect().then(success => {
-            if (success) {
-            }
-        });
+        player.connect();
     };
 }
 
 // Functions related to using the Spotify Web Player 
 // Play a specified track on the Web Playback SDK's device ID
-function play(device_id, access_token) {
+function Play(device_id, access_token) {
     $.ajax({
-    url: "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
-    type: "PUT",
-    data: '{"uris": ["spotify:track:49mu0ewiqzCYPGY3IEtNad"]}',
-    beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + access_token );},
-    success: function(data) { 
-        console.log(data)
-    }
+        url: "https://api.spotify.com/v1/me/player/play?device_id=" + device_id,
+        type: "PUT",
+        data: '{"uris": ["spotify:track:1RMJOxR6GRPsBHL8qeC2ux"]}',
+        beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + access_token );},
+        success: function(data) { 
+            console.log(data)
+        }
+    });
+}
+
+function SearchTrack(q_search, access_token){
+    $.ajax({
+        url: "https://api.spotify.com/v1/search?q=" + q_search + "&type=track&market=US&limit=10",
+        type: "GET",
+        headers: {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + access_token},
+        success: function(data) {
+            console.log(data);
+        }
     });
 }
 
 // Skip to a certain position in the song
-function skipToPosition(player, time) {
+function SkipToPosition(player, time) {
     player.seek(time * 1000).then(() => {
         console.log('Changed position!');
     });
 }
 
 // Return the length of a track in mins the number of ms
-function calculateLength(number) {
+function CalculateLength(number) {
     return "" + Math.floor(number/1000.0/60) + ":" + Math.round((number / 1000) % 60)
 }
